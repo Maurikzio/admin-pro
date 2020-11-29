894d01ae82c860fd2f1ab5270b67f952ccb7148a
MauricioM <m.morocho1196@gmail.com>
2020-11-30 00:10:31 +0200

    [UPGRADE] modifying routes to start with a certain path: dashboard/progress instead just /progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 const routes: Routes = [
   //path: '/dashboard' PagesRouting
   //path: '/auth' AuthRouting
-
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'}, //path de ruta por defecto
   { path: '**', component: NopagefoundComponent},
 ]
 
