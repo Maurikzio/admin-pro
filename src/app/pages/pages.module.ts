fd78998e6b1a7c8445372da35c9b97e5c1a159b4
MauricioM <m.morocho1196@gmail.com>
2020-11-29 18:08:06 +0200

    [UPGRADE] module for auth and notpagefound page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graph1Component } from './graph1/graph1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
-
-
 
 @NgModule({
   declarations: [
